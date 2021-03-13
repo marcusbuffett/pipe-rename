42fdae52ac810d830bd10679dee93bf119562ad3
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2021-03-13 16:16:53 +0100

    Add pretty-diff option

---
+++ src/text_diff.rs
@@ -0,0 +1,41 @@
+use diff;
+
+#[derive(Debug)]
+pub enum TextDiff {
+    Removed(String),
+    Unchanged(String),
+    New(String),
+}
+
+pub fn calculate_text_diff(old: &str, new: &str) -> Vec<TextDiff> {
+    let mut text_changes = vec![];
+
+    // diff::chars() gives us the diff char by char, so we regroup consecutive
+    // character changes into strings for simpler manipulation.
+    for diff in diff::chars(old, new) {
+        match diff {
+            diff::Result::Left(l) => {
+                if let Some(TextDiff::Removed(old)) = text_changes.last_mut() {
+                    old.push(l)
+                } else {
+                    text_changes.push(TextDiff::Removed(String::from(l)))
+                }
+            },
+            diff::Result::Both(b, _) => {
+                if let Some(TextDiff::Unchanged(unchanged)) = text_changes.last_mut() {
+                    unchanged.push(b)
+                } else {
+                    text_changes.push(TextDiff::Unchanged(String::from(b)))
+                }
+            },
+            diff::Result::Right(r) => {
+                if let Some(TextDiff::New(new)) = text_changes.last_mut() {
+                    new.push(r)
+                } else {
+                    text_changes.push(TextDiff::New(String::from(r)))
+                }
+            },
+        }
+    }
+    text_changes
+}
